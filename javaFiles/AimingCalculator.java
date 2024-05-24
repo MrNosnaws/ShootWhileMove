63eee1e0f81bc13d33b2f5080bd0f15f1b61226b
Maxwell Swanson <nosnaws1019@gmail.com>
2024-05-23 21:34:00 -0500

    new variation

--- javaFiles/AimingCalculator.java
+++ javaFiles/AimingCalculator.java
@@ -13,22 +13,34 @@ public class AimingCalculator {
         double robotAngle,
         double shooterAngle
     ) {
-        double launchVZ = Constants.TESTING.LAUNCH_SPEED * Math.sin(Math.toRadians(shooterAngle));
+        double launchVZ = Constants.TESTING.LAUNCH_SPEED / Math.sin(Math.toRadians(shooterAngle));
+        System.out.println("LVZ: " + launchVZ);
 
-        double launchVH = Constants.TESTING.LAUNCH_SPEED * Math.cos(Math.toRadians(shooterAngle));
+        double launchVH = Constants.TESTING.LAUNCH_SPEED / Math.cos(Math.toRadians(shooterAngle)); //both x and y velocitys
+        System.out.println("LVH: " + launchVH);
 
-        double launchVX = launchVH * Math.sin(Math.toRadians(robotAngle));
-        double launchVY = launchVH * Math.cos(Math.toRadians(robotAngle));
+        double launchVX = launchVH / Math.sin(Math.toRadians(robotAngle));
+        System.out.println("LVX : " + launchVX);
 
-        double xDistance = Constants.TARGET_X - robotX;
+        double launchVY = launchVH / Math.cos(Math.toRadians(robotAngle));
+        System.out.println("LVY: s" + launchVY);
 
-        double timeWhenCorrectX = xDistance / launchVX;
+        double xDistance = Constants.TARGET_X - robotX; // can be negitive
+        System.out.println("XD: " + xDistance);
+
+        boolean isParrellel = false;
+        if (launchVX == 0) {
+            isParrellel = true;
+            launchVX = 1;  //as to not give a divide by 0 error
+        }
+        double timeWhenCorrectX = xDistance / launchVX; // if negitive, than the robot is facing the wrong way
 
         boolean facingWrongWay = false;
-        double behindMultiplier = 1;
+        double behindMultiplier = 1; // this comes in late for determaning new robot angle
+
         if (timeWhenCorrectX < 0) { // this will call if its firing in the wrong direction
             facingWrongWay = true;
-            timeWhenCorrectX *= -1;
+            timeWhenCorrectX *= -1; // makes it so the math still works, acts as a reflection actross the robots Y
         }
 
         if  (xDistance < 0) {
@@ -43,15 +55,25 @@ public class AimingCalculator {
         double heightError = Constants.TARGET_HEIGHT - heightAtTime;
         double yError = Constants.TARGET_Y - yAtTime;
 
-        double newShooterAngle = shooterAngle - (heightError * Constants.TESTING.SHOOTER_ANGLE_CHANGE_MULTIPLIER);
-
-        double angleChange = yError * behindMultiplier;
-
+        double newShooterAngle;
         double newRobotAngle;
-        if (facingWrongWay) {
-            newRobotAngle = 180 + (angleChange + robotAngle);
+        if (!isParrellel) {
+            newShooterAngle = shooterAngle - (heightError * Constants.TESTING.SHOOTER_ANGLE_CHANGE_MULTIPLIER);
+
+            double angleChange = yError * behindMultiplier;
+
+            if (facingWrongWay) {
+                if (behindMultiplier == 1) { // quadrent 1
+                    newRobotAngle = (-robotAngle + 180) - angleChange;
+                } else  {
+                    newRobotAngle = (-robotAngle - 180) + angleChange;
+                }
+            } else {
+                newRobotAngle = angleChange + robotAngle;
+            }
         } else {
-            newRobotAngle = angleChange + robotAngle;
+            newShooterAngle = shooterAngle;
+            newRobotAngle = robotAngle + 45;
         }
 
         return new RobotPose(robotX, robotY, newRobotAngle, newShooterAngle);
---
+++ javaFiles/AimingCalculatorTwo.java
@@ -0,0 +1,100 @@
+package javaFiles;
+public class AimingCalculatorTwo {
+    static double dX;
+    static double dY;
+    static double dZ;
+
+    static double vX;
+    static double vY;
+
+    static double nV;
+
+    static double g;
+
+    static double h;
+    
+    public AimingCalculatorTwo () {
+
+        dX = Constants.TARGET_X - Constants.TESTING.ROBOT_X;
+        dY = Constants.TARGET_Y - Constants.TESTING.ROBOT_Y;
+        dZ = Constants.TARGET_HEIGHT;
+
+        vX = Constants.TESTING.ROBOT_VELOCITY_X;
+        vY = Constants.TESTING.ROBOT_VELOCITY_Y;
+
+        nV = Constants.TESTING.LAUNCH_SPEED;
+
+        g = 9.8;
+
+        h = 0.05;
+
+        double shooterAngle = newtonsMethod(Constants.TESTING.STARTING_SHOOTER_ANGLE, 1, 10000000);
+        double robotAngle = f(shooterAngle);
+        System.out.println("Shooter Angle : " + shooterAngle);
+        System.out.println("Robot Angle   : " + robotAngle);
+    }
+    public static double j(double x) { // x = robot angle, j(x) is an invert of f(x)
+        double top = (dX * vY) - (dY * vX);
+        double bottom = nV * (Math.cos(x) * Math.sin(x));
+        return Math.acos(top / bottom); // returns shooter angle
+    }
+
+    // Example functions
+    public static double f(double x) { // x = shooter angle
+        double top = ((dX * vY) - (dY * vX)) * Math.sqrt(2);
+        double bottom = nV * Math.cos(x);
+        double asin = Math.asin(((top / bottom) / 2));
+        return asin + (Math.PI / 4); // returns robot angle
+        // return function1 result
+    }
+
+    public static double g(double x) { // x = shooter angle
+        double innerRoot = Math.pow(nV * Math.sin(x),2) - (2 * g * dZ);
+        double outerRoot = -(nV * Math.sin(x));
+
+        double plusRoot = (outerRoot + Math.sqrt(innerRoot)) / -g;
+        double minusRoot = (outerRoot - Math.sqrt(innerRoot)) / -g;
+
+        double ans1 = Math.asin((dY - (plusRoot * vX)) / (plusRoot * nV * Math.cos(x)));
+        double ans2 = Math.asin((dY - (minusRoot * vX)) / (minusRoot * nV * Math.cos(x)));
+        if (ans1 <= ans2) {
+            return ans1;
+        } else {
+            return ans2;
+        }
+        //return function2 result
+    }
+
+    // Derivative of f(x) - g(x)
+    public static double derivative(double x) {
+        double df = (f(x + h) - f(x - h)) / (2 * h);
+        double dg = (g(x + h) - g(x - h)) / (2 * h);
+        return df - dg;
+        // f'(x) - g'(x)
+    }
+
+    // Function h(x) = f(x) - g(x)
+    public static double h(double x) {
+        return f(x) - g(x);  // (h(x) = f(x) - g(x))
+    }
+
+    // Newton's Method to find the root of h(x)
+    public static double newtonsMethod(double initialGuess, double tolerance, int maxIterations) {
+        double x = initialGuess;
+        for (int i = 0; i < maxIterations; i++) {
+            double hValue = h(x);
+            double derivativeValue = derivative(x);
+            if (Math.abs(derivativeValue) < 1e-10) {
+                System.out.println("Derivative too small, method fails.");
+                return Double.NaN;
+            }
+            double nextX = x - hValue / derivativeValue;
+            if (Math.abs(nextX - x) < tolerance) {
+                return nextX;
+            }
+            x = nextX;
+        }
+        System.out.println("Exceeded maximum iterations.");
+        return Double.NaN;
+    }
+}
